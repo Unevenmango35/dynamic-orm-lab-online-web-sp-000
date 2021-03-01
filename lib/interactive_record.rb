6785b2c040de12f67ae3682200706162c2849cd0
Rosie Choi <rosiechoimanso@gmail.com>
2021-03-01 01:25:31 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,4 +55,5 @@ class InteractiveRecord
   def self.find_by(attribute)
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.key.first} = ?"
     DB[:conn].execute(sql, attribute.values.first)
+  end
 end
